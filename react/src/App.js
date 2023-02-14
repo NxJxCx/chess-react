c047fcd93c958bac19914e1f7c25e3b8e6973f89
caneteneiljason@gmail.com <caneteneiljason@gmail.com>
2023-02-14 08:34:07 +0800

    ok

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy, Suspense } from 'react;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+function Header(props) {
+  return (
+    <header className="header navbar-top">
+      <div className="container">
+        <nav className="navbar navbar-expand-md">
+          <a className="navbar-brand mb-0 h1">Chess NJC</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#headerLinks" aria-controls="navbarController" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="headerLinks">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className={"nav-link" + (window.location.pathname === "/login"? " active": "")} href="login">Login</a>
+              </li>
+              <li className="nav-item" >
+                <a className={"nav-link" + (window.location.pathname === "/signup"? " active": "")} href="">Sign Up</a>
+              </li>
+            </ul>
+            <div className="nav navbar-nav ms-auto">
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a
-          className="App-link btn"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header authdata={null} />
+        <Routes>
+          <Route exact path="/" element={} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
