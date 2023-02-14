d9d3459dbc1ebf394fa8339388af5eaf511f6fd7
njc <caneteneiljason71967@gmail.com>
2023-02-14 11:57:40 +0800

    ok

--- react/src/App.js
+++ react/src/App.js
@@ -1,23 +1,26 @@
-import React, { lazy, Suspense } from 'react;
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 
-function Header(props) {
+const LoginPage = lazy(() => import('./components/LoginPage'));
+const SignupPage = lazy(() => import('./components/SignupPage'));
+
+const Header = () => {
   return (
     <header className="header navbar-top">
       <div className="container">
         <nav className="navbar navbar-expand-md">
-          <a className="navbar-brand mb-0 h1">Chess NJC</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#headerLinks" aria-controls="navbarController" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand mb-0 h1" href="/">Chess NJC</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarController" aria-controls="navbarController" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="headerLinks">
+          <div className="collapse navbar-collapse" id="navbarController">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
                 <a className={"nav-link" + (window.location.pathname === "/login"? " active": "")} href="login">Login</a>
               </li>
               <li className="nav-item" >
-                <a className={"nav-link" + (window.location.pathname === "/signup"? " active": "")} href="">Sign Up</a>
+                <a className={"nav-link" + (window.location.pathname === "/signup"? " active": "")} href="signup">Sign Up</a>
               </li>
             </ul>
             <div className="nav navbar-nav ms-auto">
@@ -29,14 +32,46 @@ function Header(props) {
   );
 }
 
+const ErrorPage = () => {
+  return (
+    <div className="App-header">
+      <h1 className="h1">
+        ERROR 404!
+      </h1>
+    </div>
+  );
+}
+
+const HomePage = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
+
+const LoadingScreen = () => {
+  return (
+    <div className="App-header">
+      <h1 className="h1">Loading...</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header authdata={null} />
-        <Routes>
-          <Route exact path="/" element={} />
-        </Routes>
+        <Header />
+        <Suspense fallback={<LoadingScreen />} >
+          <Routes>
+            <Route exact path="/" element={<HomePage />} >
+              <Route path="login" element={<LoginPage />} />
+              <Route path="signup" element={<SignupPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
---
+++ react/src/components/SignupPage.js
@@ -0,0 +1,14 @@
+/* Login Page */
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div classname="h1">
+        LOGIN PAGE
+      </div>
+    </>
+  );
+}
+
+export default App;
