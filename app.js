e9d9b50d20c7172b8f6f4fb5cda5fcdcc4cc83f1
NJC <caneteneiljason71967@gmail.com>
2023-02-18 09:32:24 +0800

    secure https redirection

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.enable('trust proxy');
+app.use(function(request, response, next) {
+  if (process.env.NODE_ENV === 'production' && !request.secure) {
+    return response.redirect("https://" + request.headers.host + request.url);
+  }
+  next();
+});
+
 app.use('/api/users', usersRouter);
 
 if (process.env.NODE_ENV === "production") {
