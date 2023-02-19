787f93f173e0c266f91c3eb9cc4f4507006bd91d
NJC <caneteneiljason71967@gmail.com>
2023-02-19 23:16:34 +0800

    temp

--- react/src/components/chess/Board.js
+++ react/src/components/chess/Board.js
@@ -3,7 +3,20 @@ import Square from './Square';
 function App({board, onClick=()=>{}, pieces=[], pcolors=[], clickables=[], highlights=[] }) {
   return (
   <>
-    { Array.from({length: 8*8}).fill(undefined).map((_, i) => <Square key={"square" + i} board={board} onClick={onClick} piece={pieces[i]} pcolor={pcolors[i] || "white"} x={(i % 8) + 1} y={Math.floor(i / 8) + 1}  disabled={!!!clickables[i]} highlighted={!!highlights[i]} />) }
+    { Array.from({length: 8*8}).fill(undefined)
+        .map((_, i) =>
+          <Square key={"square" + i}
+                  board={board}
+                  onClick={onClick}
+                  piece={pieces[(i % 8)][Math.floor(i / 8)]}
+                  pcolor={pcolors[(i % 8)][Math.floor(i / 8)]}
+                  x={(i % 8) + 1}
+                  y={Math.floor(i / 8) + 1}
+                  disabled={!!!clickables[(i % 8)][Math.floor(i / 8)]}
+                  highlighted={!!highlights[(i % 8)][Math.floor(i / 8)]}
+          />
+        )
+      }
   </>
   );
 }
--- react/src/components/chess/Square.js
+++ react/src/components/chess/Square.js
@@ -5,7 +5,7 @@ function App({board, onClick=()=>{}, piece, pcolor="white", disabled=true, highl
       <div className={((x + y) % 2 === 0 ? "chess-square-black" : "chess-square-white") + (disabled ? " square-disabled" : "") + (highlighted ? " square-highlighted" : "")} style={{
         width: (board.offsetWidth / 8) + "px", height: (board.offsetWidth / 8) + "px", left: ((x-1) * (board.offsetWidth / 8))  + "px", bottom: ((y-1) * (board.offsetWidth / 8)) + "px"
       }} role="button" onClick={function(e) { if (!e.currentTarget.classList.contains('square-disabled')) onClick(piece, pcolor, x, y) }}>
-        { piece ? <Piece piece={piece} color={pcolor} squareSize={board.offsetWidth / 8} /> : undefined }
+        { piece ? <Piece piece={piece} color={pcolor || "white"} squareSize={board.offsetWidth / 8} /> : undefined }
       </div>
     );
   }
