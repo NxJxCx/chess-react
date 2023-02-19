592dd7df76af85f305e6abcf1bdded81b66e5aae
njc <caneteneiljason71967@gmail.com>
2023-02-19 22:19:16 +0800

    o

--- react/src/components/ChessBoard.js
+++ react/src/components/ChessBoard.js
@@ -7,7 +7,37 @@ import Board from './chess/Board';
 
 function App() {
   const [chessboard, chessboardRef] = useState(undefined);
-  
+  const [isPlaying, setPlaying] = useState(false);
+  const [game, setGame] = useState({
+    gameData: {},
+    turnTime: 0,
+    myID: null,
+    enemyID: null,
+    selectedPiece: null,
+    get highlighted() {
+      // do some selected with highloghts 8*8 array
+      // true / false
+      if (!(this.gameData.isPlaying && this.gameData.turn === this.myID && this.selectedPiece))
+        return [] // empty array because its not our turn
+      // else do some calculations on highlighting squares on selected piece
+      let result = Array.
+      return
+    }
+    get isMyTurn() {
+      return (this.myID && this.gameData[this.myID] &&
+              this.gameData.turn === this.myID);
+    },
+    get myTimeLeft() {
+      return (this.gameData.hasStarted && this.isMyTurn() ?
+              this.gameData[this.myID].timeLeft - (Date.now() - this.turnTime) :
+              this.gameData[this.myID].timeLeft);
+    },
+    get enemyTurnLeft() {
+      return (this.gameData.hasStarted && !this.isMyTurn() ?
+              this.gameData[this.enemyID].timeLeft - (Date.now() - this.turnTime) :
+              this.gameData[this.enemyID].timeLeft);
+    }
+  });
   const onSelectPiece = (piece, color, x, y) => {
     console.log("piece: ", piece);
     console.log("color: ", color);
